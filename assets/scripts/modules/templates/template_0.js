3ff046a1ebbe996ac7da7000a3f95c94d3c34bf4
Aad <aad.pouw@gmail.com>
2023-11-23 21:17:58 +0100

    added some svg deco to the landing page

--- assets/scripts/modules/templates/template_0.js
+++ assets/scripts/modules/templates/template_0.js
@@ -1,5 +1,7 @@
 /** templates/template_0.js */
 import * as FT from './../../factory/functions.js';
 export async function template_null(){
-	return await `landing-template , todo`;
+	return await `
+		<div class='coin-deco relative'>todo</div>
+	`;
 }

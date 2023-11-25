6c9ef61980b405a648cc89d8ccfab6735a7e670f
Aad <aad.pouw@gmail.com>
2023-11-25 23:20:50 +0100

    updated a js file

--- assets/scripts/modules/templates/template_0.js
+++ assets/scripts/modules/templates/template_0.js
@@ -1,7 +1,37 @@
 /** templates/template_0.js */
 import * as FT from './../../factory/functions.js';
 export async function template_null(){
-	return await `
-		<div class='coin-deco relative'>todo</div>
+	return await `<div id='template_null' class='template-null relative display-flex'>
+		<div id='aci_cover_1' class='aci-cover relative'>
+			<div class='aci-cover-pos relative'></div>
+		</div>		
+		<div id='aci_cover_2' class='aci-cover relative'>
+			<div class='aci-cover-pos relative'></div>
+		</div>		
+		<div id='aci_cover_3' class='aci-cover relative'>
+			<div class='aci-cover-pos relative'></div>
+		</div>		
+		<div id='aci_cover_4' class='aci-cover relative'>
+			<div class='aci-cover-pos relative'></div>
+		</div>		
+		<div id='my_person' class='aci-cover relative'>
+			<div class='aci-my-person relative'></div>
+		</div>		
+		<div id='aci_cover_5' class='aci-cover relative'>
+			<div class='aci-cover-pos relative'></div>
+		</div>		
+		<div id='aci_cover_6' class='aci-cover relative'>
+			<div class='aci-cover-pos relative'></div>
+		</div>		
+		<div id='aci_cover_8' class='aci-cover relative'>
+			<div class='aci-cover-pos relative'></div>
+		</div>		
+		<div id='aci_cover_9' class='aci-cover relative'>
+			<div class='aci-cover-pos relative'></div>
+		</div>		
+		<div id='aci_cover_7' class='aci-cover relative'>
+			<div class='aci-cover-pos relative'></div>
+		</div>		
+	</div>
 	`;
 }

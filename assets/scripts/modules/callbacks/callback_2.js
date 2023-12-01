c10fc3fbda87cb5463fbe95fd195c6d3baab46ea
Aad <aad.pouw@gmail.com>
2023-12-01 22:26:30 +0100

    modiefied 3 js files in the modules/callbacks directory

--- assets/scripts/modules/callbacks/callback_2.js
+++ assets/scripts/modules/callbacks/callback_2.js
@@ -3,14 +3,12 @@ import * as FT from './../../factory/functions.js';
 import * as HL from './../../factory/handlers.js';
 export const templateTwoActions = async (_obj,page_id)=>{
 	const obj = _obj
-	//console.table({[page_id]: obj})
-	//console.log('templateTwoActions',page_id)
 	const templateTwo = await FT.getIdHelper('templateTwo')
-	//console.log('templateTwo',templateTwo)
 	await HL.detailsEventsHandler('.cookie-details','.block-one',0,5);
 	await FT.setContent('small.my-details',`awd<time><i> &#169; </i>${await FT.getYear()}</time>`,false,true);
-	await HL.detailsEventsHandler('.page-tab','.page-tabs',0,15);
+	await HL.detailsEventsHandler('.page-tab','.page-tabs',0,0);
 	await HL.detailsTabsHandler('.page-tab','.page-tabs');
+	await HL.detailsSizesHandler('.page-tab','.page-tabs','section.top',0,-110);
 	const main_elem = await FT.elQuery('main');
 	if(main_elem.classList.contains('coin-deco')){
 		main_elem.classList.remove('coin-deco');
--- assets/scripts/modules/callbacks/callback_3.js
+++ assets/scripts/modules/callbacks/callback_3.js
@@ -6,13 +6,11 @@ import * as HL from './../../factory/handlers.js';
  */
 export const templateThreeActions = async (_obj,page_id)=>{
 	const obj = _obj
-	console.table({[page_id]: obj})
-	console.log('templateThreeActions',page_id)
 	const templateThree = await FT.getIdHelper('templateThree')
-	console.log('templateThree',templateThree)
 	await HL.detailsEventsHandler('.cookie-details','.block-one',0,5);
-	await HL.detailsEventsHandler('.page-tab','.page-tabs',0,15);
+	await HL.detailsEventsHandler('.page-tab','.page-tabs',0,0);
 	await HL.detailsTabsHandler('.page-tab','.page-tabs');
+	await HL.detailsSizesHandler('.page-tab','.page-tabs','section.top',0,-110);
 	await FT.setContent('small.my-details',`awd<time><i> &#169; </i>${await FT.getYear()}</time>`,false,true);
 	const main_elem = await FT.elQuery('main');
 	if(main_elem.classList.contains('coin-deco')){

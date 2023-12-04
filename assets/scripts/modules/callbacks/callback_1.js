5a1b367133f7c0106fb214e6dfc0952cf064eb36
Aad <aad.pouw@gmail.com>
2023-12-04 17:49:25 +0100

    modified 3 js files

--- assets/scripts/modules/callbacks/callback_1.js
+++ assets/scripts/modules/callbacks/callback_1.js
@@ -1,19 +1,12 @@
-/** templates/template_1.js */
+/** callbakcs/callback_1.js */
 import * as FT from './../../factory/functions.js';
 import * as HL from './../../factory/handlers.js';
+import {shared_callbacks} from './shared_callbacks.js';
 /**
  * @description: English	
  */
 export const templateOneActions = async (_obj,page_id)=>{
 	const obj = _obj
 	const templateOne = await FT.getIdHelper('templateOne')
-	await HL.detailsEventsHandler('.cookie-details','.block-one',0,5);
-	await HL.detailsEventsHandler('.page-tab','.page-tabs',0,0);
-	await HL.detailsTabsHandler('.page-tab','.page-tabs');
-	await HL.detailsSizesHandler('.page-tab','.page-tabs','section.top',0,-110);
-	await FT.setContent('small.my-details',`awd<time><i> &#169; </i>${await FT.getYear()}</time>`,false,true);
-	const main_elem = await FT.elQuery('main');
-	if(main_elem.classList.contains('coin-deco')){
-		main_elem.classList.remove('coin-deco');
-	}
+	await shared_callbacks();
 };
--- assets/scripts/modules/callbacks/callback_2.js
+++ assets/scripts/modules/callbacks/callback_2.js
@@ -1,16 +1,9 @@
-/** templates/template_2.js */
+/** callbakcs/callback_2.js */
 import * as FT from './../../factory/functions.js';
 import * as HL from './../../factory/handlers.js';
+import {shared_callbacks} from './shared_callbacks.js';
 export const templateTwoActions = async (_obj,page_id)=>{
 	const obj = _obj
 	const templateTwo = await FT.getIdHelper('templateTwo')
-	await HL.detailsEventsHandler('.cookie-details','.block-one',0,5);
-	await FT.setContent('small.my-details',`awd<time><i> &#169; </i>${await FT.getYear()}</time>`,false,true);
-	await HL.detailsEventsHandler('.page-tab','.page-tabs',0,0);
-	await HL.detailsTabsHandler('.page-tab','.page-tabs');
-	await HL.detailsSizesHandler('.page-tab','.page-tabs','section.top',0,-110);
-	const main_elem = await FT.elQuery('main');
-	if(main_elem.classList.contains('coin-deco')){
-		main_elem.classList.remove('coin-deco');
-	}
+	await shared_callbacks();
 };

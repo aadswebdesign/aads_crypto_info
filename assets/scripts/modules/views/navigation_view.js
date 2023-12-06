f833863e0f7072d44e5b35f4f2dde35adc82be51
Aad <aad.pouw@gmail.com>
2023-12-06 08:21:12 +0100

    minor update of navigation_view.js

--- assets/scripts/modules/views/navigation_view.js
+++ assets/scripts/modules/views/navigation_view.js
@@ -5,5 +5,5 @@ export async function navigationView(){
 	await HE.div('display-none active', 'menu_item_0',null,null,false, 'nav.main-menu');
 	await HE.div('block menu-item relative', 'menu_item_1',{title:'English'},'Eng',false, 'nav.main-menu');
 	await HE.div('block menu-item flag-nl relative', 'menu_item_2',{title:'Dutch/NL'},null, false, 'nav.main-menu');
-	await HE.div('block menu-item flag-th relative', 'menu_item_3',{title:'แบบไทย'},false,false, 'nav.main-menu');
+	await HE.div('block menu-item flag-th relative', 'menu_item_3',{title:'แบบไทย'},null,false, 'nav.main-menu');
 }

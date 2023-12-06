a03a129b43ad8688df9f6d19baa6bd526f8db87e
Aad <aad.pouw@gmail.com>
2023-12-06 08:09:35 +0100

    updated navigation_view.js

--- assets/scripts/modules/views/navigation_view.js
+++ assets/scripts/modules/views/navigation_view.js
@@ -3,7 +3,7 @@ import * as HE from './../../htmlManager/htmlElements.js';
 //main-menu realtive
 export async function navigationView(){
 	await HE.div('display-none active', 'menu_item_0',null,null,false, 'nav.main-menu');
-	await HE.div('block menu-item relative', 'menu_item_1',null,'English',false, 'nav.main-menu');
-	await HE.div('block menu-item relative', 'menu_item_2',null,'Dutch/NL',false, 'nav.main-menu');
-	await HE.div('block menu-item relative', 'menu_item_3',null,'แบบไทย',false, 'nav.main-menu');
+	await HE.div('block menu-item relative', 'menu_item_1',{title:'English'},'Eng',false, 'nav.main-menu');
+	await HE.div('block menu-item flag-nl relative', 'menu_item_2',{title:'Dutch/NL'},null, false, 'nav.main-menu');
+	await HE.div('block menu-item flag-th relative', 'menu_item_3',{title:'แบบไทย'},false,false, 'nav.main-menu');
 }
